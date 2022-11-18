81f3f060bf8bf7547fa1ae99c7cff54a51259540
003565 <003565@i-pass.com.tw>
2022-11-18 17:17:00 +0800

    add 263

---
+++ Problems/_263_UglyNumber.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    internal class _263_UglyNumber
+    {
+        internal static bool isUgly(int n)
+        {
+            #region v1
+            //if (n <= 0 || n >= int.MaxValue)
+            //{
+            //    return false;
+            //}
+            //if (n == 1)
+            //{
+            //    return true;
+            //}
+            //while (true)
+            //{
+            //    if (n % 2 == 0)
+            //    {
+            //        n = n / 2;
+            //    }
+            //    if (n % 3 == 0)
+            //    {
+            //        n = n / 3;
+            //    }
+            //    if (n % 5 == 0)
+            //    {
+            //        n = n / 5;
+            //    }
+            //    if (n == 1)
+            //    {
+            //        return true;
+            //    }
+            //    if (n % 2 != 0 && n % 3 != 0 && n % 5 != 0)
+            //    {
+            //        return false;
+            //    }
+            //}
+            #endregion
+            #region v2
+            if (n == 0) return false;
+
+            while (n % 2 == 0) n = n / 2;
+
+            while (n % 3 == 0) n = n / 3;
+
+            while (n % 5 == 0) n = n / 5;
+
+            return n == 1;
+            #endregion
+        }
+    }
+}
